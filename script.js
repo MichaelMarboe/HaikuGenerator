c091284e4a28bbcee9bf85aeb23444bbcfb19cc3
Michael Marboe <michael@Michaels-iMac.local>
2021-08-03 12:10:09 +0200

    comment added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//might be easier to work with lists in lists - ie verb list with 1 list per syllabe count
 const wordList1 = ["lol", "whatever", "hi", "fun", "mate", "second", "chair", "yolanda", "mountain", "eternal"];
 const wordList2 = ["chition", "clam", "clown", "fish", "cod", "coelacanth", "conch", "copepod", "coral", "crab", "current", "scuttlefish"];
 const wordList3 = ["beat", "beautiful", "because", "become", "before", "begin", "behavior", "behind", "believe", "benefit", "billion"];
@@ -18,11 +19,13 @@ const rollLineOne = () => {
         holder.push(randomWord(wordList1));
         holder.push(randomWord(wordList2));
         holder.push(randomWord(wordList3));
+        console.log(holder);
         return holder;
     } else if (orderRandomizer === 1){
         holder.push(randomWord(wordList3));
         holder.push(randomWord(wordList2));
         holder.push(randomWord(wordList1));
+        console.log(holder);
         return holder;
     }
 }
