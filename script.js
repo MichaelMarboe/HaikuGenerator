488f40ebb9a7d0f8af7d260f42121995243b7a4f
Michael Marboe <michael@Michaels-iMac.local>
2021-08-03 11:25:23 +0200

    select random word function added

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-const oneSyllable = ["lol", "whatever", "hi", "fun", "mate", "second", "chair", "yolanda", "mountain", "eternal"]
-console.log(oneSyllable);
+const wordList1 = ["lol", "whatever", "hi", "fun", "mate", "second", "chair", "yolanda", "mountain", "eternal"]
+const wordList2 = ["chition", "clam", "clown", "fish", "cod", "coelacanth", "conch", "copepod", "coral", "crab", "current", "scuttlefish"];
+
+const randomWord = list => {
+    let randomIndex = Math.floor(Math.random()*list.length);
+    let randomWord = list[randomIndex];
+    return randomWord;
+}
