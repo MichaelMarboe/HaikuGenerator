896dedd53eb380a7a6052fdaf05e15f032529c9d
Michael Marboe <michael@Michaels-iMac.local>
2021-08-06 14:07:27 +0200

    testing html

--- README.md
+++ README.md
@@ -12,9 +12,9 @@ CHALLENGES:
 Grammatical accuracy
 Grammatical structure can't be completely accurate - it would make the project huge in scope, and each word would need a lot of additional information applied and many words would need extremely specific (and in some cases even individual) rulesets. For example, you can add an s to many nouns to make them plural but many nouns don't follow this rule. Similar variations occur across all types of words. Language is messy.
 
-Accuracy could be achieved by treating each word as an object with values that determine grammatical rules. Or, words could be crosschecked against other lists - for example, a list of words that don't take on the plural s in their plural form could be made. Then, you could check the words selected at random against those lists and add an s if they are found there. 
+Accuracy could be achieved by treating each word as an object with values that determine the conjugated forms of the word, but writing all these objects manually is more than I bargained for with this project. You'd have to sort through each word manually and determine it's rules or at least sort them and use constructor functions to generate lists of words (as objects instead of strings).
 
-However, I don't know of any way to do this that wouldn't involve manually sorting through each and every word, so I've opted not to do this. As such, this tool is not intended to deliver 100% grammatically correct sentences but rather as a way to inspire by producing random combinations of words a very strict lyrical form (the haiku's 5-7-5 syllable form). 
+But I decided against it. Instead, I made each word clickable so any user could manually replace single words. 
 
 Variation
 The generator produces sentences with random words from lists. The combinations of words are predefined combinations of nouns, verbs and adjectives. Line 1, for example, can produce a sentence that consists of: A + N + V (Adjective + Noun + Verb)("bleak" + "man" + "shivers"). This produces a linguistically correct sequence of words. 
--- script.js
+++ script.js
@@ -21,14 +21,14 @@ const nouns2 = ["gale", "blanket", "candle", "chimney", "cocoa", "fire", "flakes
 //might need to remove those that +add -ing
 //Want to add more/your own? See note about adding verbs in README > EDITING GUIDE > 2 > Verbs.  
 const verbs1 = ["walk", "run", "see", "call", "know", "say", "play"]
-const verbs2 = ["freeze", "glare", "glisten", "jingle", "shiver", "sneeze", "snuggle", "waddle", "unfold", "detain", "renew", "assert", "delay", "abandon"];
+const verbs2 = ["freeze", "glare", "glisten", "jingle", "shiver", "sneeze", "snuggle", "waddle", "unfold", "renew", "assert", "delay", "abandon"];
 
 const conjunctions1 = ["and", "but"]; //for, so removed
 
 const adverbs2 = ["calmly", "always", "almost", "tightly", "warmly", "weakly", "briefly", "fairly", "dimly", "mostly", "truly"];
 const adverbs3 = ["lively", "happily", "hastily", "restfully", "busily", "evenly", "playfully", "rarely", "cruelly", "fatally", "lazily", "knowingly"];
 
-const pronouns1 = ["he", "she"]
+const pronouns1 = ["he", "she", "it"]
 
 
 const randomWord = list => {
@@ -53,9 +53,7 @@ const randomContinuousVerb = list => {
 
 const generateLine1 = () => {
     let holder = [];
-    //2A + 1N + 2V OR 1A + 2N + 2V
-    let randomOrder = Math.floor(Math.random() * 4);
-    console.log("random id is " + randomOrder);
+    let randomOrder = Math.floor(Math.random() * 5);
     if (randomOrder === 0) {
         holder.push(randomWord(adjectives2));
         holder.push(randomWord(nouns1));
@@ -72,7 +70,10 @@ const generateLine1 = () => {
         holder.push(randomContinuousVerb(verbs2));
         holder.push(randomWord(adjectives2));
         holder.push(randomWord(nouns1));
-    }
+    } else if (randomOrder === 4){
+        //line 1 and 3 are interchangeable
+        return generateLine3();
+    }    
     console.log(holder);
     return holder;
 }
@@ -95,7 +96,7 @@ const generateLine2 = () => {
     if (randomOrder === 2){
         holder.push(randomWord(verbs2) + "s");
         holder.push(randomWord(conjunctions1));
-        holder.push(randomContinuousVerb(verbs2));
+        holder.push(randomWord(verbs2) + "s");
         holder.push(randomWord(adverbs2));
     }
     if (randomOrder === 3){
@@ -111,24 +112,33 @@ const generateLine2 = () => {
 const generateLine3 = () => {
     holder = [];
     let randomOrder = Math.floor(Math.random() * 4);
+    console.log("line3 randomorder is " + randomOrder)
     if (randomOrder === 0){
         holder.push(randomWord(pronouns1));
         holder.push(randomWord(verbs1) + "s");
         holder.push(randomWord(adverbs3));   
+    } else if (randomOrder === 1){
+        holder.push(randomWord(adverbs2));
+        holder.push(randomContinuousVerb(verbs2));
+        holder.push(randomWord(nouns1));
+    } else if (randomOrder === 2){
+        holder.push(randomContinuousVerb(verbs2));
+        holder.push(randomWord(conjunctions1));
+        holder.push(randomWord(adverbs2));
+    } else if (randomOrder === 3){
+        return generateLine1();
     }
     console.log(holder);
     return holder;
 }
 
-
-
-
 //later: make so its called from user input and once at pageload of HTML
-generateLine1();
-generateLine2();
-generateLine3();
+let line1 = generateLine1();
+let line2 = generateLine2();
+let line3 = generateLine3();
 
 //make later - push from list to innerhtml and call inside roll functions
-const pushToDOM = list => {
 
-}
+document.getElementById("line1").innerHTML = "lol.";
+
+console.log("hello");
