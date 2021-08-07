7dba6d6aa047631774cb2bf30d2199cc870909c3
MichaelMarboe <michaelmarboe@gmail.com>
2021-08-07 14:42:53 +0200

    add function to reroll words individually through search function, clean code: comments removed, code shortened, unused variables purged, arrays made pretty and easily readable

--- script2.js
+++ script2.js
@@ -1,74 +1,87 @@
-/* REMEMBER:
-
-replaceLine() replaces an entire line with a new, random sequence from lineXSequences[] - this is intentional.
-replaceWord() MUST replace a word with the same type of word as there
-
-Worklist: 
-
-1. replaceWord() = pick from referencelists. 
+/* worK:
 2. auto initialize?
 3. beautify; fix button positions, fix typography, colors, background image etc.
 4. fix wordlists, ie. conjugated words must be added and place in linesequences.
+//change name of generatePoem button after first roll to > reroll
 */
 
+/*
+These arrays hold all words that can be selected from. More words can be added easily.
+New word classes and conjugations can also be added easily - but, if new arrays (of word types/conjugations) are added, add the mew array(s) to allWords[] as well.
+NOTE: Numbers behind array names indicate syllable count in the words. Since Haiku is 5-7-5 syllables, it's important to separate them. 
+*/
+//fix syllables
+const adjectives1 = ["cold", "crisp", "dark", "gray", "harsh", "long", "numb", "wet"];
+const adjectives2 = ["bare", "barren", "bitter", "bleak", "chilling", "clear", "drafty", "foggy", "frigid", "frosty", "frozen", "hazy", "melting", "wet", "white", "windy"];
 
-const adjectives1 = ["adj2", "cold", "crisp", "dark", "gray", "harsh", "long", "numb", "wet"];
-const adjectives2 = ["adj1", "bare", "barren", "bitter", "bleak", "chilling", "clear", "drafty", "foggy", "frigid", "frosty", "frozen", "hazy", "melting", "wet", "white", "windy"];
-const nouns1 = ["n1", "chill", "man", "woman", "child", "girl", "boy", "gust", "hat", "frost", "scarf", "fog", "sled", "mist", "mug", "sled", "snow", "stew"];
-const nouns2 = ["n2", "gale", "blanket", "candle", "chimney", "cocoa", "fire", "flakes", "hail", "glove", "ice", "igloo", "indoors", "jacket", "lodge", "rain", "snowfall", "winter", "season"];
-const verbs1 = ["v1", "walk", "run", "see", "call", "know", "say", "play"]
-const verbs2 = ["v2", "freeze", "glare", "glisten", "jingle", "shiver", "sneeze", "snuggle", "waddle", "unfold", "renew", "assert", "delay", "abandon"];
-const conjunctions1 = ["c1", "and", "but"];
-const adverbs2 = ["adv2", "always", "almost", "tightly", "warmly", "weakly", "briefly", "fairly", "dimly", "mostly", "truly"];
-const adverbs3 = ["adv3", "lively", "happily", "hastily", "restfully", "busily", "evenly", "playfully", "rarely", "cruelly", "fatally", "lazily", "knowingly"];
-const pronouns1 = ["p1", "he", "she", "they", "it"];
+const nouns1 = ["chill", "man", "woman", "child", "girl", "boy", "gust", "hat", "frost", "scarf", "fog", "sled", "mist", "mug", "sled", "snow", "stew"];
+const nouns2 = ["gale", "blanket", "candle", "chimney", "cocoa", "fire", "flakes", "hail", "glove", "ice", "igloo", "indoors", "jacket", "lodge", "rain", "snowfall", "winter", "season"];
 
-//predefined possible sequences of words for each line
-//if option 2 or 3 add +ing to verb.. also s to verbs
-//easy fix - add seperate lists with conjunctions. Hard fix: inserts sequential conditions
-const line1Sequences = [[adjectives2, nouns1, verbs2], [adjectives1, nouns2, verbs2], [verbs2, adjectives1, nouns2], [verbs2, adjectives2, nouns1]];
-const line2Sequences = [[adjectives1, conjunctions1, verbs2, adverbs3], [adjectives2, conjunctions1, verbs2, adverbs2], [verbs2, conjunctions1, verbs2, adverbs2], [verbs2, adverbs2, verbs2, adjectives1]];
-const line3Sequences = [[pronouns1, verbs1, adverbs3], [adverbs2, verbs2, nouns1], [verbs2, conjunctions1, adverbs2]];
+const verbs1 = ["walk", "run", "see", "call", "know", "say", "play"]
+const verbs2 = ["freeze", "glare", "glisten", "jingle", "shiver", "sneeze", "snuggle", "waddle", "unfold", "renew", "assert", "delay", "abandon"];
 
-let wordHolder = [[], [], []];
-let repickReferences = [[], [], []];
+const conjunctions1 = ["and", "but"];
 
-//used to replace a single word
+const adverbs2 = ["always", "almost", "tightly", "warmly", "weakly", "briefly", "fairly", "dimly", "mostly", "truly"];
+const adverbs3 = ["lively", "happily", "hastily", "restfully", "busily", "evenly", "playfully", "rarely", "cruelly", "fatally", "lazily", "knowingly"];
+
+const pronouns1 = ["he", "she", "they", "it"];
+
+/*
+Sequences of words can be added here. Possible sequences are predefined, NOT random.  
+These are all syntactically correct, but there's room for more. Note that sentences may still be nonsensical albeit syntactically possible. That's poetry!
+*/
+const line1Sequences = [
+    [adjectives2, nouns1, verbs2], 
+    [adjectives1, nouns2, verbs2], 
+    [verbs2, adjectives1, nouns2], 
+    [verbs2, adjectives2, nouns1]
+];
+
+const line2Sequences = [
+    [adjectives1, conjunctions1, verbs2, adverbs3], 
+    [adjectives2, conjunctions1, verbs2, adverbs2], 
+    [verbs2, conjunctions1, verbs2, adverbs2], 
+    [verbs2, adverbs2, verbs2, adjectives1]];
+
+const line3Sequences = [
+    [pronouns1, verbs1, adverbs3], 
+    [adverbs2, verbs2, nouns1], 
+    [verbs2, conjunctions1, adverbs2]
+];
+
+/* 
+Used to hold sequences when an entire line of words is pushed to the DOM.
+Do not edit array structure manually.
+*/
+const wordHolder = [[], [], []];
+
+/*
+Necessary for search function that replaces a single word with a word from the origin array.
+See function: replaceWord(wordNumber) 
+*/
 const allWords = [adjectives1, adjectives2, nouns1, nouns2, verbs1, verbs2, conjunctions1, adverbs2, adverbs3, pronouns1];
 
-//can be used both for replacing references and pushing to references initially
-//second is optional parameter, if used = will replace with array at given index
-const pushToReferences = (array, replaceAtindex) => {
-   if (replaceAtindex != undefined){
-        repickReferences.splice(replaceAtindex, 1, array);
-   } else {
-        repickReferences.push(array); 
-   } 
-}
-
+/*
+Returns a random word from a passed array.
+*/
 const getRandomWordFrom = array => {
     let randomIndex = Math.floor(Math.random()*array.length);
-    //roll again if random index is 0
-    if (randomIndex === 0){
-        return getRandomWordFrom(array);
-    } else {
-        return array[randomIndex]; 
-    }
+    return array[randomIndex]; 
 }
 
-//conjugation issue...
+/*
+Generates and replaces an entire line of words based on predefined word sequences in lineSequences[].
+*/  
 const generateLine = lineNumber => {
-    //reset of this array in order to update it
-    repickReferences[lineNumber] = [];
+    //Reset the array within wordHolder[] that holds the given line.
     wordHolder[lineNumber] = []
-    let wordIncrement = 0;
-    //separate for line reroll functionality
+    //could be shorter - but how do you pick sequences?
     if (lineNumber === 0){
         clearLineDOM(0);
         let randomIndex = Math.floor(Math.random()*line1Sequences.length);
         line1Sequences[randomIndex].forEach(nestedArray => {
             wordHolder[0].push(getRandomWordFrom(nestedArray));
-            repickReferences[0].push(nestedArray);
         })
         pushLineToDOM(0);
     } else if (lineNumber === 1){
@@ -76,7 +89,6 @@ const generateLine = lineNumber => {
         let randomIndex = Math.floor(Math.random()*line2Sequences.length);
         line2Sequences[randomIndex].forEach(nestedArray => {
             wordHolder[1].push(getRandomWordFrom(nestedArray));
-            repickReferences[1].push(nestedArray);
         })
         pushLineToDOM(1);
     } else if (lineNumber === 2){
@@ -84,18 +96,27 @@ const generateLine = lineNumber => {
         let randomIndex = Math.floor(Math.random()*line3Sequences.length);
         line3Sequences[randomIndex].forEach(nestedArray => {
             wordHolder[2].push(getRandomWordFrom(nestedArray));
-            repickReferences[2].push(nestedArray);
         })
         pushLineToDOM(2);
     }
-    
 }
 
-const replaceWord = word => {
-    
+/*
+1. Search through allWords[...] to find the array that holds the word (from the innerHTML) on the passed line. 
+2. When a match is found in an array, replace with a word from this array and stop. 
+*/
+const replaceWord = (wordNumber) => {
+    let wordString = document.getElementById("word"+wordNumber.toString()).innerHTML;
+    allWords.forEach(array => {
+        if (array.indexOf(wordString) >= 1){
+            wordString = getRandomWordFrom(array);
+            document.getElementById("word" + wordNumber.toString()).innerHTML = getRandomWordFrom(array);
+            return;
+        }
+    });
 }
 
-//--- DOM FUNCTIONS ---
+// --- DOM FUNCTIONS --- 
 
 const clearLineDOM = lineNumber => {
     let line = document.getElementsByClassName("line" + lineNumber.toString());
@@ -119,8 +140,11 @@ const pushLineToDOM = lineNumber => {
     }
 }
 
-//this doubles as "reroll all button" (!!)
-const initialize = () => {
+/*
+Doubles as an initializer and as total reroll button. 
+Clears all lines and rerolls sequences and words.  
+*/
+const rollAllLines = () => {
     generateLine(0);
     generateLine(1);
     generateLine(2);
